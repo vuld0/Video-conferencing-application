de006e5329580219af67b8430fd56b1683ac0477
vuld0 <chiragsonubablani@karunya.edu.in>
2021-02-04 18:54:47 +0530

    Bug fixing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 	res.redirect(`/${uuidv4()}`);
