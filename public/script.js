a7e4982feb104afb0581fa821756c54b628dac45
Naveen <naveensreevalsan@gmail.com>
2021-02-04 22:26:20 +0530

    Some modifications

--- public/script.js
+++ public/script.js
@@ -1,20 +1,24 @@
 const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
+const videoGrid = document.getElementById('video-grid') // this is to connect to the ejs file
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '3030'
+  port: '3030'  // port number associated,  will be changed to 443 for hosting 
 })
 let myVideoStream;
-const myVideo = document.createElement('video')
+const myVideo = document.createElement('video') // temporary video file
 myVideo.muted = true;
 const peers = {}
+
+// code below will give the stream of video and audio
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  myVideoStream = stream;
+  myVideoStream = stream; 
   addVideoStream(myVideo, stream)
+  
+  // answering to the call from the second user, so that he can also see my feed.
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
@@ -23,6 +27,7 @@ navigator.mediaDevices.getUserMedia({
     })
   })
 
+  // listening to the broadcast message from the server
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
@@ -43,15 +48,20 @@ navigator.mediaDevices.getUserMedia({
   })
 })
 
+// listening to the broadcast message from the server
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
 })
 
+// mypeer is used for peer to peer connection
 myPeer.on('open', id => {
+// for second user to join the room
   socket.emit('join-room', ROOM_ID, id)
 })
 
 function connectToNewUser(userId, stream) {
+
+// the second user need to start a call and send their video stream
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
@@ -64,12 +74,13 @@ function connectToNewUser(userId, stream) {
   peers[userId] = call
 }
 
+// function to add the video to the ejs file
 function addVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
-    video.play()
+    video.play() //  play the video
   })
-  videoGrid.append(video)
+  videoGrid.append(video) // appending to the ejs file
 }
 
 
